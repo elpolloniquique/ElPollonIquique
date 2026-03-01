8d907f8bae2e417f3a88b83c00d1eb273a8e82aa
tutacanehuillca-cmyk <tutacanehuillca@gmail.com>
2026-03-01 12:30:24 -0300

    ssdcdc

--- js/app.js
+++ js/app.js
@@ -263,7 +263,7 @@ const products = {
     { name: "Salchipapas", description: "", price: 6700, image: "img/salchipapa.png" }
   ],
   "agregados": [
-    { name: "1 Pollo entero solo", description: "1 pollo entero", price: 15000, image: "img/pollo solo.png" },
+    { name: "1 Pollo entero solo", description: "1 pollo solo", price: 15000, image: "img/pollo solo01.png" },
     { name: "1/2 Pollo solo", description: " 1/2 pollo  -  parte truto y pechuga", price: 9900, image: "img/medio pollo solo.png" },
     { name: "1/4 pollo solo", description: "1/4 de polo -- truto o pechuga ---segun el stock", price: 5800, image: "" },
     { name: "Porcion de papas fritas familiar", description: "Porción grande de papas crujientes", price: 9000, image: "img/porcion de papa.png" },
